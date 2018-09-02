d94c267a034e63823876309c53e412572c4666e6
Mateus Santana <mateus.isajessi@gmail.com>
2018-09-02 10:28:48 -0300

    Atualização 02/09/2018

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 
 (function(){
 
+	// menu hamburguer
+
 	var $menu = document.querySelector('.menu');
 	var $ul = document.querySelector('.nav__ul');
 
@@ -8,4 +10,28 @@
 		$ul.classList.toggle('active')
 	});
 
+	// contato validação
+
+	var $nome = document.querySelector('#nome');
+	var $email = document.querySelector('#email');
+	var $assunto = document.querySelector('#assunto');
+	var $msg = document.querySelector('#msg');
+	var $emailLabel = document.querySelector('.js-email span'); 
+
+	var padraoEmail = /^\w+[\.-]?\w+?@\w+\.\w+\.?\w{2}?/;
+
+	$email.addEventListener('input', function(){
+		var email = this.value;
+
+		if(padraoEmail.test(email)){
+			$email.style.borderColor = 'rgba(34, 200, 175, 1)';
+			$emailLabel.style.display = 'none';
+		}else{
+			$email.style.borderColor = 'red';
+			$emailLabel.style.display = 'inline';
+			$emailLabel.style.color = 'red';
+		}
+	})
+
 })();
+
